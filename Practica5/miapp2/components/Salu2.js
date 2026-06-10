c2c561e7551ec66dc9aabba40eda1fa72a95b4e7
Saul <123047731@upq.edu.mx>
2026-06-09 18:37:55 -0600

    Practica No.6: Estructura y Componentes

---
+++ Practica5/miapp2/components/Salu2.js
@@ -0,0 +1,16 @@
+
+import { Button, Text, Image, View } from "react-native";
+
+export const Salu2 = () => {
+    return (
+
+        <view>
+
+            <Text>Soy un componente propio</Text>
+            <Image source={require('../assets/wave.png')}/>
+            <Button title="Hola-206"></Button>
+
+        </view>
+
+    )
+}
