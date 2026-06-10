30e4eec323475a30f52b5c90962a0245d3f89254
Saul <123047731@upq.edu.mx>
2026-06-10 01:56:59 -0600

    Perfil práctica 6
    Creación de componente propio de perfil para la práctica 6

---
+++ Practica5/miapp2/components/Perfil.js
@@ -0,0 +1,15 @@
+import { Text } from 'react-native';
+
+export const Perfil = () => {
+    return (
+
+        <view>
+
+            <Text>Soy un componente propio</Text>
+            <Text>Soy un componente propio</Text>
+            <Text>Soy un componente propio</Text>
+
+        </view>
+
+    )
+}
